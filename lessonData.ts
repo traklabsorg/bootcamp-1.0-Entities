17f68f5de3c6b23b50b39e425d4fc9920a6f6af0
debu3223 <debabratamukherjee@trakinvest.com>
2021-03-02 11:22:33 +0530

    changes in LessonData Made

--- lessonData.ts
+++ lessonData.ts
@@ -28,6 +28,9 @@ export class LessonData extends EntityBase{
   @Column({ name: 'lesson_id',nullable:false})
   lessonId: number;
 
+  @Column({name: 'total_points',nullable:true})
+  totalPoints: number;
+
   @ManyToOne((type) => Lesson, lesson => lesson.lessonData, {
     onDelete: 'CASCADE',onUpdate: 'RESTRICT'
   })
