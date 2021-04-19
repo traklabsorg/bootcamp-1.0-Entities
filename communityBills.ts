99dcaa6b031e446b222c7b227749fafd2ff7abfd
Bhaskar Katragadda <katragaddabhaskar1729@gmail.com>
2021-04-19 12:22:03 +0530

    modified entity-> communitybill

--- communityBills.ts
+++ communityBills.ts
@@ -4,37 +4,37 @@ import { EntityBase } from "./submodules/platform-3.0-Framework/EntityBase/Entit
 
 @Entity("communityBills")
 export class CommunityBills extends EntityBase{
-    @Column({name:'communityId'})
+    @Column({name:'communityId',nullable:true)
     communityId:number;
 
-    @Column({name:'bill_date'})
+    @Column({name:'bill_date',nullable:true})
     billDate:Date;
 
-    @Column({name:'title'})
+    @Column({name:'title',nullable:true})
     title:string;
 
     @Column({name:'description',nullable:true})
     description:string;
 
-    @Column({name:'total_users'})
+    @Column({name:'total_users',nullable:true})
     totalUsers:number;
 
-    @Column({name:'per_user_fee'})
+    @Column({name:'per_user_fee',nullable:true})
     perUserFee:number;
 
-    @Column({name:'tax'})
+    @Column({name:'tax',nullable:true})
     tax:number;
 
-    @Column({name:'total_charges'})
+    @Column({name:'total_charges',nullable:true})
     totalCharges:number;
 
-    @Column({name:'additional_details',type:"json"})
+    @Column({name:'additional_details',type:"json",nullable:true})
     additionalDetails:string;
 
-    @Column({name:'is_paid'})
+    @Column({name:'is_paid',nullable:true})
     isPaid:boolean;
 
-    @Column({name:'invoice_url'})
+    @Column({name:'invoice_url',nullable:true})
     invoiceUrl:string;
 
     @ManyToOne((type)=>Community, community => community.Id,{
