1ce9b9e7e6f2d4830761f392ce2195aa4245389e
AAYUSH SOMANI <aayush@trakinvest.com>
2021-04-23 17:47:20 +0530

    removed unique url constraint

--- liveContent.ts
+++ liveContent.ts
@@ -6,49 +6,48 @@ import { UserMeetingProvider } from "./userMeetingProvider";
 import { MeetingProvider } from "./meetingProvider";
 
 @Entity("liveContents")
-@Unique(["url"])
 @Unique(["userId"])
-export class LiveContent extends EntityBase{
+export class LiveContent extends EntityBase {
 
-  @Column({ name: 'url',nullable:true })
+  @Column({ name: 'url', nullable: true })
   url: string;
 
-  @Column({ name: 'content_details',nullable:true, type:"json" })
+  @Column({ name: 'content_details', nullable: true, type: "json" })
   contentDetails: string;
 
 
-  @Column({ name: 'webinar_details',nullable:true, type:"json" })
+  @Column({ name: 'webinar_details', nullable: true, type: "json" })
   webinarDetails: string;
 
-  @Column({ name: 'host_users',nullable:true, type:"json" })
+  @Column({ name: 'host_users', nullable: true, type: "json" })
   hostUsers: string;
 
-  @Column({ name: 'type_of_registration',nullable:true })
+  @Column({ name: 'type_of_registration', nullable: true })
   typeOfRegistration: string;
 
-  @Column({ name: 'start_date_time',nullable:true })
+  @Column({ name: 'start_date_time', nullable: true })
   startDateTime: Date;
 
-  @Column({ name: 'duration',nullable:true })
+  @Column({ name: 'duration', nullable: true })
   duration: number;
 
-  @Column({ name: 'end_date',nullable:true })
+  @Column({ name: 'end_date', nullable: true })
   endDate: Date;
 
-  @Column({ name: 'recording_url',nullable:true })
+  @Column({ name: 'recording_url', nullable: true })
   recordingURL: string;
 
-  @Column({ name: 'external_meeting_provider_user_id',nullable:false})
+  @Column({ name: 'external_meeting_provider_user_id', nullable: false })
   externalMeetingProviderUserId: string;
 
-  @Column({ name: 'meeting_provider_id',nullable:false})
+  @Column({ name: 'meeting_provider_id', nullable: false })
   meetingProviderId: number;
 
-  @Column({ name: 'user_id',nullable:false})
+  @Column({ name: 'user_id', nullable: false })
   userId: number;
 
   @ManyToOne((type) => User, user => user.liveContent, {
-    onDelete: 'CASCADE',onUpdate: 'RESTRICT'
+    onDelete: 'CASCADE', onUpdate: 'RESTRICT'
   })
 
   @JoinColumn({ name: 'user_id' })
@@ -56,13 +55,13 @@ export class LiveContent extends EntityBase{
 
 
   @ManyToOne((type) => MeetingProvider, meetingProvider => meetingProvider.liveContent, {
-    onDelete: 'CASCADE',onUpdate: 'RESTRICT'
+    onDelete: 'CASCADE', onUpdate: 'RESTRICT'
   })
   @JoinColumn({ name: 'meeting_provider_id' })
   meetingProvider: MeetingProvider;
 
   @OneToMany((type) => LiveContentUser, liveContentUser => liveContentUser.liveContent, {
-		onDelete: 'CASCADE',onUpdate: 'RESTRICT'
+    onDelete: 'CASCADE', onUpdate: 'RESTRICT'
   })
   liveContentUser: LiveContentUser[]
 
